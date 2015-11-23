72412eead6cc66d184588e2a2987cc389ba8176a
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-23 20:14:34 +0100

    Stub for new project: clj-fileutils

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-fileutils
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_fileutils/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-fileutils.core-test
+  (:require [clojure.test :refer :all]
+            [clj-fileutils.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

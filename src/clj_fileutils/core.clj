d105ac9616e0505a76ef9567fa83fd2be129b356
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-24 20:57:51 +0100

    Renamed core module to fileutils, added licence text(s) to this module

--- src/clj_fileutils/core.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-fileutils.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
--- test/clj_fileutils/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-fileutils.core-test
-  (:require [clojure.test :refer :all]
-            [clj-fileutils.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

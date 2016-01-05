fcaf671a6f7a91de18c629109bb91faf2d32759f
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-01-05 21:18:14 +0100

    Library does not need to have main function

--- project.clj
+++ project.clj
@@ -15,7 +15,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot clj-fileutils.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.6.0"]])

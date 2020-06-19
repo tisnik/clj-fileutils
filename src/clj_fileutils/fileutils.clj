4ca3522bea5c2adee54002cecd3bb98ff44f3de0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-19 08:42:02 +0200

    Reformatted fileutils.clj and updated comments

--- src/clj_fileutils/fileutils.clj
+++ src/clj_fileutils/fileutils.clj
@@ -12,97 +12,110 @@
 
 (ns clj-fileutils.fileutils)
 
-(require '[hozumi.rm-rf    :as rm-rf])
+(require '[hozumi.rm-rf :as rm-rf])
 
 (def temporary-name-prefix
-    "clojure-fileutils-")
+  "Prefix for all temporary files."
+  "clojure-fileutils-")
 
 (defn print-slurp-exception
-    "Print warning message to the standard output."
-    ([filename]
-        (println "Warning: cannot read content of the following file:" filename))
-    ([directory filename]
-        (println "Warning: cannot read content of the following file:" (str directory "/" filename))))
+  "Print warning message to the standard output."
+  ([filename]
+   ; variant with just filename
+   (println "Warning: cannot read content of the following file:" filename))
+  ([directory filename]
+   ; variant with directory and filename
+   (println "Warning: cannot read content of the following file:"
+            (str directory "/" filename))))
 
 (defn slurp-
-    "Alternative (slurp) implementation that does not throw an exception, but returns nil instead."
-    ([filename]
-        (try
-            (slurp filename)
-            (catch Exception e (print-slurp-exception filename))))
-    ([directory filename]
-        (try
-            (slurp (str directory "/" filename))
-            (catch Exception e (print-slurp-exception directory filename)))))
+  "Alternative (slurp) implementation that does not throw an exception, but returns nil instead."
+  ([filename]
+   ; variant with just filename
+   (try (slurp filename)
+        (catch Exception e (print-slurp-exception filename))))
+  ([directory filename]
+   ; variant with directory and filename
+   (try (slurp (str directory "/" filename))
+        (catch Exception e (print-slurp-exception directory filename)))))
 
 (defn new-file
-    "Just a shortcut for (new java.io.File filename)."
-    ( [filename]
-    (new java.io.File filename))
-    ( [path filename]
-    (new java.io.File path filename)))
+  "Just a shortcut for (new java.io.File filename)."
+  ([filename]
+   ; variant with just filename
+   (new java.io.File filename))
+  ([path filename]
+   ; variant with path and filename
+   (new java.io.File path filename)))
 
 
 (defn remove-temporary-directory
-    "Remove (delete) temporary directory that resides in /tmp (at least on Linux)."
-    [directory-name]
-    (let [file (new java.io.File directory-name)
-          abs-path (.getAbsolutePath file)]
-        ; make sure we are not going to remove wrong directory!
-        (if (.startsWith abs-path "/tmp")
-            (rm-rf/rm-r file))))
+  "Remove (delete) temporary directory that resides in /tmp (at least on Linux)."
+  [directory-name]
+  (let [file (new java.io.File directory-name)
+        abs-path (.getAbsolutePath file)]
+    ; make sure we are not going to remove wrong directory!
+    (if (.startsWith abs-path "/tmp")
+      (rm-rf/rm-r file))))
 
 (defn remove-directory
-    [directory]
-    (rm-rf/rm-r directory))
+  "Remove specified directory and all its content (recursively)."
+  [directory]
+  (rm-rf/rm-r directory))
 
 (defn mv-file
-    "Move or rename one file. On the same filesystem the rename should be atomic."
-    [filename1 filename2]
-    (let [file1 (new java.io.File filename1)
-          file2 (new java.io.File filename2)]
-          (.renameTo file1 file2)))
+  "Move or rename one file. On the same filesystem the rename should be atomic."
+  [filename1 filename2]
+  (let [file1 (new java.io.File filename1)
+        file2 (new java.io.File filename2)]
+    (.renameTo file1 file2)))
 
 (defn move-file
-    "Move or rename one file. On the same filesystem the rename should be atomic."
-    [filename1 filename2]
-    (mv-file filename1 filename2))
+  "Move or rename one file. On the same filesystem the rename should be atomic."
+  [filename1 filename2]
+  (mv-file filename1 filename2))
 
 (defn make-temporary-log-file-name
-    []
-    (let [basedir-name (new java.io.File (System/getProperty "java.io.tmpdir"))
-          base-name    temporary-name-prefix
-          temp-file    (java.io.File/createTempFile base-name ".log" basedir-name)]
-          (.getAbsolutePath temp-file)))
+  "Special variant of function to make temporary file name that will reside in
+  (at least on Linux)."
+  []
+  (let [basedir-name (new java.io.File (System/getProperty "java.io.tmpdir"))
+        base-name temporary-name-prefix
+        temp-file (java.io.File/createTempFile base-name ".log" basedir-name)]
+    (.getAbsolutePath temp-file)))
 
 (defn make-temporary-directory
-    "Make temporary directory that would reside in /tmp (at least on Linux)."
-    []
-    (let [basedir-name (System/getProperty "java.io.tmpdir")
-          base-name    (str temporary-name-prefix (System/currentTimeMillis))
-          temp-dir     (new java.io.File basedir-name base-name)]
-        (.mkdir temp-dir)
-        temp-dir))
+  "Make temporary directory that would reside in /tmp (at least on Linux)."
+  []
+  (let [basedir-name (System/getProperty "java.io.tmpdir")
+        base-name (str temporary-name-prefix (System/currentTimeMillis))
+        temp-dir  (new java.io.File basedir-name base-name)]
+    (.mkdir temp-dir)
+    temp-dir))
 
 (defn filename-list
-    "Return list of file names that resides in specified directory."
-    (   [directory]
-        (->> (.listFiles (new java.io.File directory))
-             (filter #(.isFile %))
-             (map #(.getName %))))
-    (   [directory extension]
-        (->> (.listFiles (new java.io.File directory))
-             (filter #(.isFile %))
-             (map #(.getName %))
-             (filter #(.endsWith % extension)))))
+  "Return list of file names that resides in specified directory."
+  ([directory]
+   ; variant with just directory
+   (->> (.listFiles (new java.io.File directory))
+        (filter #(.isFile %))
+        (map #(.getName %))))
+  ([directory extension]
+   ; variant with directory and extension
+   (->> (.listFiles (new java.io.File directory))
+        (filter #(.isFile %))
+        (map #(.getName %))
+        (filter #(.endsWith % extension)))))
 
 (defn file-list
-    "Return list of files that resides in specified directory."
-    (   [directory]
-        (->> (.listFiles (new java.io.File directory))
-             (filter #(.isFile %))))
-    (   [directory extension]
-        (->> (.listFiles (new java.io.File directory))
-             (filter #(.isFile %))
-             (filter #(.endsWith (.getName %) extension)))))
+  "Return list of files that resides in specified directory."
+  ([directory]
+   ; variant with just directory
+   (->> (.listFiles (new java.io.File directory))
+        (filter #(.isFile %))))
+  ([directory extension]
+   ; variant with directory and extension
+   (->> (.listFiles (new java.io.File directory))
+        (filter #(.isFile %))
+        (filter #(.endsWith (.getName %) extension)))))
 
